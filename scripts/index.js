a540787a7fa2df099daddf10f4bbccc5e004552a
dmtof <77462675+dmtof@users.noreply.github.com>
2022-12-02 14:36:59 +0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+/* let pointItemDot = document.querySelectorAll('.point-item');
+
+for (let i = 0; i < pointItemDot.length; i++) {
+    pointItemDot[i].addEventListener('click', function(num) {
+        document.getElementById(`pointItem${i}`).classList.toggle('choosen-point')}, false);
+} */
